3d8d4b5334d7d86fa240821f988cb9c7eb5e300d
jcbondsii <jcbondsii@gmail.com>
2026-04-09 19:58:01 -0500

    Changed files to match solution and have clearer statements.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,7 @@
-from textnode import *
+from textnode import TextNode, TextType
 
 def main():
-    textB = TextNode("This is some bold text", TextType.BOLD)
-    textI = TextNode("This is an italic text", TextType.ITALIC)
-    textC = TextNode("This is some code text", TextType.CODE)
-    textL = TextNode("This is some anchor text", TextType.LINK, url="https://www.boot.dev/")
-    print(textB.__repr__())
-    print(textI.__repr__())
-    print(textC.__repr__())
-    print(textL.__repr__())
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev/")
+    print(node)
 
-if __name__ == "__main__":
-    main()
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 class TextType(Enum):
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -8,29 +9,17 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode:
-    def __init__(self, text: str, text_type: TextType, url: str = None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
+    
+    def __eq__(self, other):
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
 
-    def render(self) -> str:
-        if self.text_type == TextType.BOLD:
-            return f"**{self.text}**"
-        elif self.text_type == TextType.ITALIC:
-            return f"_{self.text}_"
-        elif self.text_type == TextType.CODE:
-            return f"`{self.text}`"
-        elif self.text_type == TextType.LINK:
-            return f"[{self.text}]({self.url})"
-        elif self.text_type == TextType.IMAGE:
-            return f"![{self.text}]({self.url})"
-        else:
-            return self.text
-    
-    def __eq__(self, value):
-        if not isinstance(value, TextNode):
-            return False
-        return self.text == value.text and self.text_type == value.text_type and self.url == value.url
-    
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
