e526dec056e48dd941e233e011ab6f7e6d9d6cf8
jcbondsii <jcbondsii@gmail.com>
2026-04-19 20:19:57 -0500

    Added block markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -87,3 +87,13 @@ def text_to_textnodes(text):
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
     return nodes
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
