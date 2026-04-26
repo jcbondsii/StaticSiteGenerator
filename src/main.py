e39459bdf1f455725909ce0f6234511ad73352c1
jcbondsii <jcbondsii@gmail.com>
2026-04-25 22:36:10 -0500

    Added a multi page generator.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
-from copy import copy_folder
+import os
+import shutil
+
+from copystatic import copy_files_recursive
 from page_generator import generate_page
 
-def main():
-    source = "./content/index.md"
-    destination = "./public/index.html"
-    template = "./template.html"
-    generate_page(source, template, destination)
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
-if __name__ == "__main__":
-    main()
+
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
+
+
+main()
--- src/page_generator.py
+++ src/page_generator.py
@@ -1,35 +1,34 @@
-from markdown_blocks import BlockType, block_to_block_type, markdown_to_blocks, markdown_to_html_node
+import os
+from markdown_blocks import markdown_to_html_node
 
-def extract_title(markdown):
-    blocks = markdown_to_blocks(markdown)
-    for block in blocks:
-        block_type = block_to_block_type(block)
-        if block_type == BlockType.HEADING:
-            return block[2:].strip()
-    raise ValueError("No heading found in markdown")
 
 def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using template {template_path}")
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
 
-    # Read the contents of the markdown file
-    with open(from_path, 'r') as f:
-        markdown_content = f.read()
-    
-    # Read the contents of the template file
-    with open(template_path, 'r') as f:
-        template_content = f.read()
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
 
-    # convert markdown to html
-    html_content = markdown_to_html_node(markdown_content)
-    html_content = html_content.to_html()
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
 
-    # grab the title from the markdown
     title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
 
-    # replace the title and content in the template
-    template_content = template_content.replace("{{ Title }}", title)
-    template_content = template_content.replace("{{ Content }}", html_content)
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
 
-    # Write the generated HTML to the destination file
-    with open(dest_path, 'w') as f:
-        f.write(template_content)
+
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
